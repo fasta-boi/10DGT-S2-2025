bc5895948f714c618b42cde7cba78f6dfc8ae6c7
Fasta_Boi <lachlan.gaeth@kingsway.school.nz>
2025-10-08 15:00:13 +1300

    Variables version 1.1

---
+++ Dgt_Python_Variables.py
@@ -0,0 +1,27 @@
+#Lesson 1
+# = variables
+greetings = "Hello World"
+print(greetings)
+
+# Store a number
+my_number = 80 # Assign 80 to the variable called my_number
+print(my_number)
+
+my_number = 7
+print(my_number)
+
+# Assign th value of one variable to another
+my_number = greetings
+print(my_number)# make variable name suit contents
+
+''' Use three quotation mazrks to make one big comment '''
+
+# Create new variable
+num_1 = 5
+num_2 = 18
+
+sum_1 = num_1 + num_2
+print(sum_1)
+
+# print fromating
+print(f"My calcutlation for adding {num_1} and { num_2} is {sum_1}.")
